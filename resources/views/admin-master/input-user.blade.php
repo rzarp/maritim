10deafe37ab40c3ef58076e5b7e1cb97636bc50b
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-03-14 16:23:40 +0700

    Update input-user.blade.php

--- resources/views/admin-master/input-user.blade.php
+++ resources/views/admin-master/input-user.blade.php
@@ -4,7 +4,7 @@
         <h1>Input User</h1>        
     </div>
 
-    {{-- @if (session()->has('pesan'))
+    @if (session()->has('pesan'))
     <div class="alert alert-success alert-dismissible show fade">
     <div class="alert-body">
         <button class="close" data-dismiss="alert">
@@ -13,9 +13,9 @@
         {{ session()->get('pesan') }}
     </div>
     </div>
-    @endif --}}
+    @endif
 
-    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+    <form action="{{ route('store.user') }}" method="post" enctype="multipart/form-data" class="form-horizontal">
         @csrf                                
     <div class="row">
 <div class="col-12 col-md-12 col-lg-12">
@@ -27,20 +27,45 @@
                   <input name="user_id" type="hidden" value="">
                     <div class="form-group">
                       <label>Name</label>
-                      <input type="text" name="name" class="form-control" placeholder="Name" required>
+                      <input type="text" name="name" class="form-control" placeholder="Name" required value="{{old ('name')}}">
+                      @error('name')
+                          <div class="text-danger">
+                              {{ $message}}
+                          </div>
+                      @enderror
                     </div>
                      <div class="form-group">
                       <label>Email</label>
-                      <input type="email" name="email" class="form-control" placeholder="Name" required>
+                      <input type="email" name="email" class="form-control" placeholder="Name" required value="{{old ('email')}}">
+                      @error('email')
+                          <div class="text-danger">
+                              {{ $message}}
+                          </div>
+                      @enderror
                     </div>
-                    
                     <div class="form-group">
-                      <label>Kategori</label>
-                      <select class="form-control">
-                        <option>0</option>
-                        <option>1</option>
+                      <label for="password">Password</label>
+                      <input type="password" name="password" class="form-control" placeholder="Password" value="{{old ('password')}}">
+                      @error('password')
+                          <div class="text-danger">
+                              {{ $message}}
+                          </div>
+                      @enderror
+                  </div>
+                    <div class="form-group">
+                      <label for="is_admin">Is_Admin</label>
+                      <select name="is_admin" class="form-control">
+                          <option value=""></option>
+                          @foreach($is_admin as $admin)
+                          <option value="{{ $admin }}" {{ (old ('is_admin') == $admin ) ? 'selected' : '' }}>{{ $admin }}</option>
+                          @endforeach
+                      @error('is_admin')
+                          <div class="text-danger">
+                              {{ $message}}
+                          </div>
+                      @enderror
                       </select>
-                    </div>
+                      </div>
                     
                     <div class="form-group">
                         <button class="btn btn-danger" type="reset" value="Reset">Reset</button>
